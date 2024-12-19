b8607f6ac8439e595e47a79e6b8afdd5e1c08b9f
troy1eighty2 <troytran000@gmail.com>
2024-12-18 23:18:36 -0600

    stuff

--- today.py
+++ today.py
@@ -1,4 +1,6 @@
 import sqlite3
+import datetime
+from datetime import date
 import argparse
 
 
@@ -17,19 +19,24 @@ def createDatabase():
     conn.close()
 
 
-def argparse():
+def parseInput():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument()
-    pass
+    parser.add_argument('habit', type=str,
+                        help='the str habit you are logging')
+    parser.add_argument('status', type=int,
+                        help='1 for complete, 0 for incomplete')
+
+    args = parser.parse_args()
+    return args
 
 
-def submission():
+def submission(args):
     conn = sqlite3.connect("habits.db")
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO habits (habit, date, status) 
-        VALUES ()
+        VALUES (args.habit, , args.status)
     """)
     conn.commit()
     conn.close()
@@ -42,7 +49,10 @@ def displayMap():
 
 def main():
     # createDatabase()
-    displayMap()
+    # displayMap()
+    args = parseInput()
+    date = datetime.datetime.today()
+    print(date)
 
 
 if __name__ == "__main__":
