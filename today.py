62fe7d7b4553f4f83fa6ca0bc4bcea084e168fb5
troy1eighty2 <troytran000@gmail.com>
2024-12-19 18:11:40 -0600

    random

--- today.py
+++ today.py
@@ -8,11 +8,17 @@ def createDatabase():
     conn = sqlite3.connect("habits.db")
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS habits(
+        CREATE TABLE IF NOT EXISTS log(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             habit TEXT NOT NULL,
             date TEXT NOT NULL,
             status INTEGER
+        )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS habits(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            habit TEXT NOT NULL
         )
     """)
     conn.commit()
@@ -31,12 +37,18 @@ def parseInput():
     return args
 
 
+editList():
+    pass
+
+
 def submission(args):
+    todays_date = datetime.datetime.today()
     conn = sqlite3.connect("habits.db")
     cursor = conn.cursor()
+
     cursor.execute("""
-        INSERT INTO habits (habit, date, status) 
-        VALUES (args.habit, , args.status)
+        INSERT INTO log (habit, date, status)
+        VALUES (args.habit, todays_date, args.status)
     """)
     conn.commit()
     conn.close()
@@ -47,12 +59,19 @@ def displayMap():
     black_square = "\033[30m■\033[0m"  # Black square
 
 
+def editChoices():
+
+
 def main():
     # createDatabase()
     # displayMap()
     args = parseInput()
-    date = datetime.datetime.today()
-    print(date)
+    todays_date = datetime.datetime.today()
+    date_object = datetime.date(
+        todays_date.year, todays_date.month, todays_date.day)
+    print(date_object)
+    print(args.habit)
+    print(args.status)
 
 
 if __name__ == "__main__":
